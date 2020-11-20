9a600aee541d92eed7943b443a54877788c6d6be
kaiquec3 <74798358+kaiquec3@users.noreply.github.com>
2020-11-20 20:42:14 -0300

    Add files via upload

---
+++ Exercício 13/Exercício 13/Class1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercício_13
+{
+    class Triangulo
+    {
+        double lado1;
+        double lado2;
+        double lado3;
+
+        public void SetLado1(double p)
+        {
+            lado1 = p;
+        }
+
+        public void SetLado2(double p)
+        {
+            lado2 = p;
+        }
+
+        public void SetLado3(double p)
+        {
+            lado3 = p;
+        }
+
+        public double GetLado1()
+        {
+            return lado1;
+        }
+
+        public double GetLado2()
+        {
+            return lado2;
+        }
+
+        public double GetLado3()
+        {
+            return lado3;
+        }
+
+        public int VerificarSeETriangulo()
+        {
+            int r;
+
+            if (lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1) r = 1;
+            else r = 0;
+
+            return r;
+        }
+
+        public string DeterminarTipoDeTriangulo()
+        {
+            string r;
+            if (VerificarSeETriangulo() == 1)
+            {
+                if (lado1 == lado2 && lado1 == lado3) r = "equilátero";
+                else if ((lado1 == lado2 && lado1 != lado3) || (lado1 == lado3 && lado1 != lado2) ||
+                (lado2 == lado3 && lado2 != lado1)) r = "isósceles";
+                else r = "escaleno";
+            }
+            else r = "Não é um triângulo";
+
+            return r;
+        }
+
+        public string VerificarSeERetangulo()
+        {
+            string s = "";
+
+            if (VerificarSeETriangulo() == 1)
+            {
+                if (Math.Pow(lado1, 2) + Math.Pow(lado2, 2) == Math.Pow(lado3, 2)
+                    || Math.Pow(lado1, 2) + Math.Pow(lado3, 2) == Math.Pow(lado2, 2)
+                    || Math.Pow(lado2, 2) + Math.Pow(lado3, 2) == Math.Pow(lado1, 2))
+                {
+                    s = "Este triângulo é retângulo";
+                }
+                else s = "Este triângulo não é retângulo";
+            }
+            else s = "Não é triângulo";
+
+            return s;
+
+            
+        }
+    }
+}
---
+++ Exercício 13/Exercício 13/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercício_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new Triangulo();
+            double k;
+
+            do
+            {
+                Console.Write("Informe o valor do primeiro lado do triângulo: ");
+                k = double.Parse(Console.ReadLine());
+
+                if (k > 0) a1.SetLado1(k);
+            }
+            while (k <= 0);
+
+            do
+            {
+                Console.Write("Informe o valor do segundo lado do triângulo: ");
+                k = double.Parse(Console.ReadLine());
+
+                if (k > 0) a1.SetLado2(k);
+            }
+            while (k <= 0);
+
+            do
+            {
+                Console.Write("Informe o valor do terceiro lado do triângulo: ");
+                k = double.Parse(Console.ReadLine());
+
+                if (k > 0) a1.SetLado3(k);
+            }
+            while (k <= 0);
+
+            Console.WriteLine(a1.VerificarSeERetangulo());
+        }
+    }
+}
